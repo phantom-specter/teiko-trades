a15dbac31af1420f676e1816d53ec197a07f7f27
UbongJacob <ubongjacob13@gmail.com>
2025-01-10 18:23:02 +0100

    latest changes

--- utils/time.ts
+++ utils/time.ts
@@ -3,5 +3,4 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
-
 export const getRelativeTimePass = (time: string) => dayjs(time).fromNow();
